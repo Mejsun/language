961f7e68ae9ae991b4b8adaef0198c00d22bf047
Mejsun <88291174+Mejsun@users.noreply.github.com>
2022-03-07 17:05:20 +0000

    Add files via upload
    updating javascript functionality for toggling submenu items

--- app.js
+++ app.js
@@ -1,84 +1,66 @@
 const rainbow = document.querySelector("header");
-const oldDiv = document.querySelector('div.subscribe');
 const subscribed = document.querySelector('.send');
 const newDiv = document.querySelector('div.thanks');
 const btnScrollToTop = document.querySelector(".scrollBack");
-const containerIt = document.querySelector('div.containerIt');
-const containerFr = document.querySelector('div.containerFr');
-const containerAr = document.querySelector('div.containerAr');
-const containerO = document.querySelector('div.containerO');
-const cardIt = document.querySelector('div.italian');
-const cardFr = document.querySelector('div.french');
-const cardAr = document.querySelector('div.arabic');
-const cardO = document.querySelector('div.other');
-const miNouns = document.querySelector('.miNouns');
-const miVerbs = document.querySelector('.miVerbs')
-const containerNounsIt = document.querySelector('div.containerNounsIt');
-const containerVerbsIt = document.querySelector('div.containerVerbsIt');
-const menuContainer = document.querySelector(".menu");
-const items = menuContainer.querySelectorAll("li");
-const input= document.querySelectorAll('.input')
+const card = document.querySelectorAll('.card')
+const menuItem = document.querySelectorAll('.menuItem')
 
+//changing background on mousem moving
 rainbow.addEventListener("mousemove",MouseEvent);
 function MouseEvent(e){
   e.preventDefault();
   rainbow.style.backgroundColor = "rgb("+e.offsetX+","+e.offsetY+", 100)"
 }
 
+//scroll back to top
 btnScrollToTop.addEventListener("click", function(){
   window.scrollTo({
     top:0, left:0, behavior:"smooth"
-  })
-})
+  });})
 
+//show thank you div after subscribing
 subscribed.addEventListener("click", function(){
   newDiv.style.display='block';})
 
-cardIt.addEventListener("click",function(cardIt){
-  containerIt.style.display='block';
-  containerFr.style.display='none';
-  containerAr.style.display='none';
-  containerO.style.display='none'
-})
-cardFr.addEventListener("click",function(cardFr){
-  containerFr.style.display='block';
-  containerIt.style.display='none';
-  containerAr.style.display='none';
-  containerO.style.display='none'
-})
-cardAr.addEventListener("click",function(cardAr){
-  containerAr.style.display='block';
-  containerIt.style.display='none';
-  containerFr.style.display='none';
-  containerO.style.display='none'
-})
-cardO.addEventListener("click",function(cardO){
-  containerO.style.display='block';
-  containerAr.style.display='none';
-  containerIt.style.display='none';
-  containerFr.style.display='none';
-})
+//toggle language cards submenu
+card.forEach( l => {
+    l.addEventListener('click', (e) =>{
+      e.preventDefault();
+      let currentTab = document.querySelector(`.container${l.href.substring(l.href.indexOf('#'))}`);
+      if(currentTab.classList.contains("hide")){
+        document.querySelectorAll('.container').forEach(t => {
+          t.classList.add("hide");
+        });
+        currentTab.classList.remove("hide");
+      } else {
+        document.querySelectorAll('.container').forEach(t => {
+          t.classList.add("hide");
+        }); }
+  })})
 
-miNouns.addEventListener('click', () =>{
-  if(containerNounsIt.style.display === "none"){
-    containerNounsIt.style.display="block";
-    containerVerbsIt.style.display="none";
-  }else{
-    containerNounsIt.style.display="none";
-  }
-});
+//separate function for each grammar field
 
-miVerbs.addEventListener('click', () =>{
-  if(containerVerbsIt.style.display === "none"){
-    containerVerbsIt.style.display="block";
-    containerNounsIt.style.display="none";
-  }else{
-    containerVerbsIt.style.display="none";
-  }
-});
 
-for (let i = 0; i < items.length; i++) {
-  items[i].addEventListener('click', function() {
+  menuItem.forEach( l => {
+    l.addEventListener('click', (e) =>{
+      if(!l.classList.contains('othertext')){
+        e.preventDefault();
+        let currentItem = document.querySelector(`.submenu${l.href.substring(l.href.indexOf('#'))}`);
+        if(currentItem.classList.contains("hide")){
+          document.querySelectorAll('.submenu').forEach(t => {
+            t.classList.add("hide");
+          });
+          currentItem.classList.remove("hide");
+        } else {
+          document.querySelectorAll('.submenu').forEach(t => {
+            t.classList.add("hide");
+          });} 
+        }
+    })})
+
+  //show which submenu item is active
+  for (let i = 0; i < menuItem.length; i++) {
+  menuItem[i].addEventListener('click', function() {
     const current = document.getElementsByClassName("active");
 
     if (current.length > 0) {
